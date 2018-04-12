f1b579c806341e79f34da91a4bcc41458709bd4a
zuodrifter <1217102313@qq.com>
2018-04-12 19:38:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/thread/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.thread;
+
+import android.annotation.SuppressLint;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public static final int UPDATE_TEXT = 1;
+    TextView text;
+    Button changeText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = findViewById(R.id.text);
+        changeText = findViewById(R.id.chang_text);
+        changeText.setOnClickListener(this);
+    }
+
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case UPDATE_TEXT:
+                    text.setText("Nice to meet you!");
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.chang_text:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Message message = new Message();
+                        message.what = UPDATE_TEXT;
+                        handler.sendMessage(message);
+                    }
+                }).start();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
+        /*移除消息队列中所有消息和所有的Runnable*/
+    }
+}
